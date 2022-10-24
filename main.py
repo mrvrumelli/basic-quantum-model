b99a775892a5f643db1ce0ef9b140d91998594c5
simpleUser02 <mrumellihn@gmail.com>
2022-10-24 18:04:53 +0300

    comment line for unnecessary lines

--- main.py
+++ main.py
@@ -22,18 +22,17 @@ def equations_of_neurons(x: array,s,l,L):
     a = -(2*math.pow(math.pi*s*x[1],2))/(4*math.pow(math.pi,2)*math.pow(s,4)+math.pow(l*L,2))
     first_layer = x[0]*np.exp(a+1j*(math.pi/(l*L)+(2*math.pi*math.pow(s,2)*a)/(l*L)))
     middle_layer = (1-1j*math.exp())
-    # last_layer =
 
-def predict(s, x, X, H, h, bias):
-    """
-    s: scale value
-    x: slit position
-    X: Transpose of x
-    H: Quadratic weight
-    h: Weight
-    complex # i = j in python (1j)
-    """
-    return prediction 
+# def predict(s, x, X, H, h, bias):
+#     """
+#     s: scale value
+#     x: slit position
+#     X: Transpose of x
+#     H: Quadratic weight
+#     h: Weight
+#     complex # i = j in python (1j)
+#     """
+#     return prediction 
 
 
 
