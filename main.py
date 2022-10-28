12c05eac2bea96e3a43b52f89a9e62e1c250e6db
simpleUser02 <mrumellihn@gmail.com>
2022-10-28 16:18:38 +0300

    forward propagation

--- main.py
+++ main.py
@@ -12,33 +12,30 @@ def activation_function(prediction):
     """ 
     return pow(abs(np.exp(prediction)),2)
 
-def equations_of_neurons(x: array,x_next: array, s, l, L, beta):
+def predict(x: array,x_next: array, s, l, L, beta, a_i: float, layer):
     """
     x: input array
     s: sigma value of quation
     l: lamda value of equation
     L: distance between layers
+    a_i: last layer last item
+    complex # i = j in python (1j)
     """
-    a = -(2*math.pow(math.pi*s*x[1],2))/(4*math.pow(math.pi,2)*math.pow(s,4)+math.pow(l*L,2))
-    first_layer = x[0]*np.exp(a+1j*(math.pi/(l*L)+(2*math.pi*math.pow(s,2)*a)/(l*L)))
-    middle_layer = (1-1j*math.exp((-math.pi*math.pow(x-x_next,2))/(2*math.pi*beta**2+1j*l*L)))/math.sqrt(-2j+(l*L/math.pi*beta**2))
-
-# def predict(s, x, X, H, h, bias):
-#     """
-#     s: scale value
-#     x: slit position
-#     X: Transpose of x
-#     H: Quadratic weight
-#     h: Weight
-#     complex # i = j in python (1j)
-#     """
-#     return prediction 
-
-
-
-
-    #prediction = np.dot(pow(s,2)* np.dot(X, H), x) + s * np.dot(X, h) + bias
-    #3. sayfadaki buyuk formul
-    prediction = activation_function(prediction)
+    if layer == 'first': 
+        a = -(2*math.pow(math.pi*s*x[1],2))/(4*math.pow(math.pi,2)*math.pow(s,4)+math.pow(l*L,2))
+        prediction = x[0]*np.exp(a+1j*(math.pi/(l*L)+(2*math.pi*math.pow(s,2)*a)/(l*L)))
+    elif layer == 'middle':
+        prediction = (1-1j*math.exp((-math.pi*math.pow(x-x_next,2))/(2*math.pi*beta**2+1j*l*L)))/math.sqrt(-2j+(l*L/math.pi*beta**2))
+    else: 
+        prediction = (1-1j*math.exp((-math.pi*math.pow(x-a_i,2))/(2*math.pi*beta**2+1j*l*L)))/math.sqrt(-2j+(l*L/math.pi*beta**2))
     
+    prediction = activation_function(prediction)
     return prediction
+
+def forward_propagation(x,x_next, s, l, L, beta, a_i, layer, y):
+
+    y_pred = predict(x,x_next, s, l, L, beta, a_i, layer)
+    loss = (y_pred - y)**2   
+    d_loss = 2*(y_pred - y)
+    
+    return y_pred, loss, d_loss
